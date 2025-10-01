300c64e1657b496e127ab95332df8d4e83b636dd
BLANK-ben <beze984@gmail.com>
2025-10-01 13:09:01 +0100

    Add files via upload

---
+++ DAY 9/dictionary_in_list_exercise.py
@@ -0,0 +1,24 @@
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["paris", "little", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    }
+]
+#Do Not change the code above
+
+#TODO: Write the function that will allow new country to be added to travel_log.
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = country_visited
+    new_country["visits"] = times_visited
+    new_country["cities"] = cities_visited
+    travel_log.append(new_country)
+#Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint petersburg"])
+print(travel_log)
